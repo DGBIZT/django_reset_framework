082889073bdd7619f8b653ccd76fb9383b1c90b0
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-09-18 16:19:11 +0300

    практика сериализация

---
+++ vehicle/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from vehicle.apps import VehicleConfig
+from vehicle.views import CarViewSet
+
+app_name = VehicleConfig.name
+
+router = DefaultRouter()
+router.register(r'cars',CarViewSet, basename='cars')
+
+
+
+urlpatterns = [
+
+] + router.urls
---
+++ vehicle/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from vehicle.serliazers import CarSerializer
+from vehicle.models import Car
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
+
