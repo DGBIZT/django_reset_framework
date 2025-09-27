acc7a840ee889b62a4d6e2151e8a440cf5448d4a
DGBIZT <georgiy.gorlov@gmail.com>
2025-09-27 19:23:30 +0300

    Права доступа в DRF

--- vehicle/views.py
+++ vehicle/views.py
@@ -4,6 +4,7 @@ from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets, status, generics
 from rest_framework.permissions import IsAuthenticated
 
+from vehicle.permissions import IsOwnerOrStaff
 from vehicle.serliazers import CarSerializer, MotoSerializer, MilageSerializers, MotoMilageSerializers, MotoCreateSerializer
 from vehicle.models import Car, Moto, Milage
 from django.shortcuts import get_object_or_404
@@ -33,6 +34,12 @@ class CarViewSet(viewsets.ModelViewSet):
 
 class MotoCreateAPIView(generics.CreateAPIView):
     serializer_class = MotoCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        new_moto = serializer.save()
+        new_moto.owner = self.request.user
+        new_moto.save()
 
 class MotoListAPIView(generics.ListAPIView):
     serializer_class = MotoSerializer
@@ -45,6 +52,7 @@ class MotoRetrieveAPIView(generics.RetrieveAPIView):
 class MotoUpdateAPIView(generics.UpdateAPIView):
     serializer_class = MotoSerializer
     queryset = Moto.objects.all()
+    permission_classes = [IsOwnerOrStaff]
 
 class MotoDestroyAPIView(generics.DestroyAPIView):
     queryset = Moto.objects.all()
